de4648b14279ecc92c1090b84ba6fe24b8e0dbcd
Griffin Brown <browgrif@gmail.com>
2022-05-24 23:58:35 -0400

    people styling

--- Models/ResultsViewModel.cs
+++ Models/ResultsViewModel.cs
@@ -9,19 +9,18 @@ namespace SwapiMVC.Models
 {
     public class ResultsViewModel<TResult>
     {
-        [JsonPropertyName("results")]
-        public IEnumerable<TResult> Results { get; set; }
-    }
-    public class ResultsViewModel
-    {
+        
         [JsonPropertyName("count")]
         public int Count { get; set; }
         [JsonPropertyName("next")]
         public string Next { get; set; }
         [JsonPropertyName("previous")]
         public string Previous { get; set; }
-
+        
+        [JsonPropertyName("results")]
+        public IEnumerable<TResult> Results { get; set; }
         public string NextPageNum => Next?.Split("?page=").LastOrDefault();
         public string PreviousPageNum => Previous?.Split("?page=").LastOrDefault();
+
     }
 }
