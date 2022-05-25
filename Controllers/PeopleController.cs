80b85ad3770db208d9ca60cbcffb9076eee1b7a0
Griffin Brown <browgrif@gmail.com>
2022-05-24 22:52:57 -0400

    People Controller

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 
 namespace SwapiMVC.Controllers
 {
     public class PeopleController : Controller
     {
+        private readonly HttpClient _httpClient;
+        public PeopleController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("swapi");
+        }
         public IActionResult Index()
         {
             
